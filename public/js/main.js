f5bc04de6d3a99095983741345743e9415c3b69b
Ha IB. Nguyen Trung <hant52@user.techcombank.com.vn>
2018-11-03 12:04:41 +0700

    hant

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+class KhoaPham extends React.Component {
+    render() {
+        return <h1 className = "mauvang" > Demo Reactjs </h1>;
+    }
+}
+
+ReactDOM.render( < KhoaPham / > , document.getElementById("root"));
